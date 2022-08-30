a99b821c11b1997af90a2b5e2d5098868fe1b36d
Ruchan Yalçın <67342076+ruchany13@users.noreply.github.com>
2022-08-30 22:59:26 +0300

    add something

--- newlinux.sh
+++ newlinux.sh
@@ -11,11 +11,13 @@ sudo apt-get install \
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
+sudo apt-get update
+
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo docker run hello-world
 
